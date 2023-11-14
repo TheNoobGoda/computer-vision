45ab07a59008ca313a0283f6e61dfa45c332c33d
sofiaavelino <sofiaavelino22@gmail.com>
2023-11-14 15:24:58 +0000

    segmentation

--- exp/vc_exp.py
+++ exp/vc_exp.py
@@ -14,13 +14,22 @@ while True:
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
             for id, lm in enumerate(handLms.landmark):
+                
+                # Get the actual depth (z-coordinate) using the depth map
+                depth_value = results.multi_hand_landmarks[0].landmark[id].z
+
+                # Convert normalized depth to actual depth in millimeters
+                actual_depth = int(depth_value * 1000)
+
                 h, w, c = image.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
+                print(cx, cy, actual_depth)
 
-            if id == 20 :
+            if id == 4 :
                 cv2.circle(image, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
 
             mpDraw.draw_landmarks(image, handLms, mpHands.HAND_CONNECTIONS)
+            cv2.putText(image, str(actual_depth), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
 
             cv2.imshow("Output", image)
             cv2.waitKey(1)
