f00f20125d8826128e9e646b44816f5622890f79
TheNoobGoda <davidespinhamarques@gmail.com>
2023-11-15 21:54:58 +0000

    trying

--- exp/lineDetection.py
+++ exp/lineDetection.py
@@ -1,23 +1,12 @@
 import cv2
 import numpy as np
 
-def remove_color(img):
-    new_img = img.copy()
-    new_img[np.where((new_img < [50,50,50]).all(axis=2))] = [0,0,0]
-    return new_img
 
 img = cv2.imread('img/akai2.jpeg')
 img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-img_gray = cv2.cvtColor(img_gray,cv2.COLOR_GRAY2BGR)
-black = remove_color(img_gray)
-cv2.imwrite('img/lineDetection/black.jpg',black)
+img_blur = cv2.GaussianBlur(img_gray,(3,3), sigmaX=0, sigmaY=0)
 
-
-img_blur = cv2.GaussianBlur(img,(3,3), sigmaX=0, sigmaY=0)
-
-cv2.imwrite('img/lineDetection/gray.jpg',img_gray)
-
-edges = cv2.Canny(image=img_blur, threshold1=100, threshold2=200) 
+edges = cv2.Canny(img_blur, 100, 200) 
 sobel = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5)
 cv2.imwrite('img/lineDetection/sobel.jpg',sobel)
 cv2.imwrite('img/lineDetection/canny.jpg',edges)
