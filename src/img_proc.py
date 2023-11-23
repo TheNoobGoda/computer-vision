3b574d63a34990df6cb1f5fc9d7647bb5b645fd4
TheNoobGoda <davidespinhamarques@gmail.com>
2023-11-23 15:21:45 +0000

    added kbd extraction to pipeline

--- src/img_proc.py
+++ src/img_proc.py
@@ -43,3 +43,32 @@ class ImgProc:
         thresh[np.all(thresh == (255,255,255),axis=-1)] = (255,0,0)
         thresh[np.all(thresh == (0,0,0),axis=-1)] = (0,255,0)
         cv2.imwrite(dest_img_path,thresh)
+
+    def find_piano(image_path,dest_img_path = 'img/cropped_keyboard.jpg'):
+        # Read the input image
+        original_image = cv2.imread(image_path)
+        blur = cv2.GaussianBlur(original_image,(15,15),50)
+
+        # Convert the image to HSV color space
+        hsv_image = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+
+        # Define a mask for the color range of white keys
+        lower_white = np.array([0, 0, 200])
+        upper_white = np.array([180, 30, 255])
+        mask_white = cv2.inRange(hsv_image, lower_white, upper_white)
+
+        piano_image = cv2.bitwise_and(original_image, original_image, mask=mask_white)
+        cv2.imshow('Piano Image', piano_image)
+
+        # Find contours in the white mask
+        contours, _ = cv2.findContours(mask_white, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        # Find the bounding box of the combined contours
+        x, y, w, h = cv2.boundingRect(np.vstack(contours))
+
+        # Crop the original image to the bounding box
+        cropped_image = original_image[y:y+h, x:x+w]
+
+        cv2.imwrite(dest_img_path, cropped_image)
+
+        return cropped_image
--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from img_proc import ImgProc
 
 def main():
-    hull = ImgProc.convexHull('img/pressedkeyboard.png')
-    ImgProc.crop_img('img/pressedkeyboard.png',hull)
+    # hull = ImgProc.convexHull('img/pressedkeyboard.png')
+    # ImgProc.crop_img('img/pressedkeyboard.png',hull)
+    ImgProc.find_piano('img/keyboard_exemples/kbd1.jpeg')
     ImgProc.img_thresh('img/cropped_keyboard.jpg')
 
 
