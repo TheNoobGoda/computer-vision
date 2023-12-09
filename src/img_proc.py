a68fd3eae6e8dece11aee403f07e1aa17d8260ec
TheNoobGoda <davidespinhamarques@gmail.com>
2023-12-09 00:26:23 +0000

    removed convexhull function

--- src/img_proc.py
+++ src/img_proc.py
@@ -3,33 +3,6 @@ import numpy as np
 
 class ImgProc:
 
-    def convexHull(src_img_path, dest_img_path = 'img/results/ConvexHull.jpg'):
-        # Load the image
-        img = cv2.imread(src_img_path)
-        # Convert it to greyscale
-        grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        blur = cv2.GaussianBlur(grey,(7,7),sigmaX=500,sigmaY=500)
-        # Threshold the image
-        ret, thresh = cv2.threshold(blur,190,255,cv2.THRESH_BINARY)
-        # cv2.imwrite('img/results/thresh.jpg',thresh)
-        # Find the contours
-        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        # For each contour, find the convex hull and draw it
-        # on the original image.
-
-        hull = []
-        for i in range(len(contours)):
-            for j in range(len(contours[i])):
-                hull.append(contours[i][j])
-        hull = np.array(hull)
-        hull = cv2.convexHull(hull)
-
-        x,y,w,h = cv2.boundingRect(hull)
-        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-        # Display the final convex hull image
-        # cv2.imwrite(dest_img_path, img)
-        return hull
-
     def crop_img(src_img_path,hull, dest_img_path = 'img/results/cropped_keyboard.jpg'):
         img = cv2.imread(src_img_path)
         x,y,w,h = cv2.boundingRect(hull)
